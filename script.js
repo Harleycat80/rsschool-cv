794f65d15440131a95e268c69bd01e9a8ef1a0e9
Harleycat80 <Harleycat@rambler.ru>
2022-03-03 14:24:05 +0400

    refactor:add path to picture

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 
-const header=document.querySelector("ul")
-const main = document.querySelector("main")
+const header=document.querySelector('ul')
+const main = document.querySelector('main')
 
-let area=""
+let area=''
 
 
 function changeBack(e){   
     if(area==""){
     area = e.target.textContent
     let fild =document.getElementById(area)
-     fild.classList.add("active")
+     fild.classList.add('active')
    
     }
     else{
     fild = document.getElementById(area) 
-    fild.classList.remove("active")
+    fild.classList.remove('active')
     area = e.target.textContent
     fild =document.getElementById(area)
-    fild.classList.add("active")
+    fild.classList.add('active')
     
    }
    
 }
 
 function changeStatus(){
-    let activ = document.getElementsByClassName("active")
-    activ.classList.remove("active")
+    let activ = document.getElementsByClassName('active')
+    activ.classList.remove('active')
 
 }
 
-header.addEventListener("click",changeBack)
-main.addEventListener("click",changeStatus)
+header.addEventListener('click',changeBack)
+main.addEventListener('click',changeStatus)
